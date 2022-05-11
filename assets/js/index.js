eb5c642ab5aefba5e232abb7225373423674aaf3
BadyAndriy <67754253+BadyAndriy@users.noreply.github.com>
2022-05-11 15:25:34 +0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+  $('.header__menu-burger').click(function(){
+    $('body').toggleClass('open')
+  })
+});
+// =====================================================================================
+//in case js in turned off
+$(window).on('load', function () {
+  $(".header").removeClass("scroll")
+});
+$(window).scroll(function () {
+  var sc = $(window).scrollTop()
+  if (sc > 0) {
+    $(".header").addClass("scroll")
+  } else {
+    $(".header").removeClass("scroll")
+  }
+});
+// =====================================================================================
+var header = $('.header'),
+  scrollPrev = 0;
+$(window).scroll(function() {
+  var scrolled = $(window).scrollTop();
+  if ( scrolled > 100 && scrolled > scrollPrev ) {
+    header.addClass('out');
+  } else {
+    header.removeClass('out');
+  }
+  scrollPrev = scrolled;
+});
+// =====================================================================================
+new Swiper('.first-slider',{
+
+  // крапки навігації
+  pagination: {
+    el: '.first__pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // включення/виключення
+  // свайп
+  simulateTouch: true,
+
+  // чутливість
+  touchRatio: 1,
+
+  // кут спрацьовування свайпа
+  touchAngel: 45,
+
+  // курсор перетягування
+  grabCursor: false,
+
+  // переключення по кліку на слайд
+  slideToClickedSlide: false,
+
+  // управління клавіатурою
+  keyboard: {
+    // вкл\викл
+    enable: true,
+    // вкл\викл
+    // тільки коли слайдер в межах вюпорта
+    onlyInViewport: true,
+    // вкл\викл
+    // управління клавішами PgUp, PgDown
+    pageUpDown: true,
+  },
+
+  // автовисота
+  autoHeight: false,
+
+  // кількість слайдів для показу
+  slidesPerView: 1,
+
+  // відключення функції slidesPerView
+  // якщо слайдів менше ніж треба
+  watchOverflow: true,
+
+  // відступи між слайдами
+  spaceBetween: 0,
+
+  // кількість пролистуваних слайдів
+  slidesPerGroup: 1,
+
+  // активний слайд по центру
+  centeredSlides: true,
+
+  // стартовий слайд
+  initialSlide: 0,
+
+  // нескінечний слайдер
+  loop: true,
+
+  // вільний режим перегортання слайдів
+  freeMode: false,
+
+  // скорість переключення слайдів
+  speed: 300,
+
+  // автопрограння слайдів
+  autoplay: {
+    delay: 5000,
+  },
+
+});
