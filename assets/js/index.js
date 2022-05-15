9ab3c80e0f23b4f56f63d48f8922002339b5735b
BadyAndriy <67754253+BadyAndriy@users.noreply.github.com>
2022-05-15 19:35:29 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,3 +103,93 @@ new Swiper('.first-slider',{
   },
 
 });
+// =====================================================================================
+new Swiper('.seventh-slider',{
+
+  navigation: {
+    nextEl: '.seventh__button-next',
+    prevEl: '.seventh__button-prev',
+  },
+
+  // включення/виключення
+  // свайп
+  simulateTouch: true,
+
+  // чутливість
+  touchRatio: 1,
+
+  // кут спрацьовування свайпа
+  touchAngel: 45,
+
+  // курсор перетягування
+  grabCursor: false,
+
+  // переключення по кліку на слайд
+  slideToClickedSlide: false,
+
+  // управління клавіатурою
+  keyboard: {
+    // вкл\викл
+    enable: true,
+    // вкл\викл
+    // тільки коли слайдер в межах вюпорта
+    onlyInViewport: true,
+    // вкл\викл
+    // управління клавішами PgUp, PgDown
+    pageUpDown: true,
+  },
+
+  // автовисота
+  autoHeight: false,
+
+  // кількість слайдів для показу
+  slidesPerView: 1,
+
+  // відключення функції slidesPerView
+  // якщо слайдів менше ніж треба
+  watchOverflow: true,
+
+  // відступи між слайдами
+  spaceBetween: 0,
+
+  // кількість пролистуваних слайдів
+  slidesPerGroup: 1,
+
+  // активний слайд по центру
+  centeredSlides: true,
+
+  // стартовий слайд
+  initialSlide: 0,
+
+  // нескінечний слайдер
+  loop: true,
+
+  // вільний режим перегортання слайдів
+  freeMode: false,
+
+  // скорість переключення слайдів
+  speed: 300,
+
+  thumbs: {
+    swiper: {
+      el: '.seventh__mini-slider',
+      // стартовий слайд
+      initialSlide: 0,
+      // відключення функції slidesPerView
+      // якщо слайдів менше ніж треба
+      watchOverflow: true,
+      breakpoints: {
+        320: {
+          slidesPerView: 3,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+    }
+  },
+
+});
