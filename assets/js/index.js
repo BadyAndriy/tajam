001941d8674fddcdea987c44a182068e97222083
BadyAndriy <67754253+BadyAndriy@users.noreply.github.com>
2022-05-23 16:06:08 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,18 +16,193 @@ $(window).scroll(function () {
     $(".header").removeClass("scroll")
   }
 });
-// =====================================================================================
-var header = $('.header'),
-  scrollPrev = 0;
-$(window).scroll(function() {
-  var scrolled = $(window).scrollTop();
-  if ( scrolled > 100 && scrolled > scrollPrev ) {
-    header.addClass('out');
-  } else {
-    header.removeClass('out');
-  }
-  scrollPrev = scrolled;
+$(document).ready(function(){
+    $("#header__menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 500);
+  });
 });
+// =====================================================================================
+// Проверка видимости элемента (в видимой части страницы)Достаточно,
+// чтобы верхний или нижний край элемента был виден
+function isVisible(elem) {
+  let coords = elem.getBoundingClientRect();
+  let windowHeight = document.documentElement.clientHeight;
+  // видны верхний ИЛИ нижний край элемента
+  let topVisible = coords.top > 0 && coords.top < windowHeight;
+  let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+  return topVisible || bottomVisible;
+}
+function showVisible() {
+  for (let img of document.querySelectorAll('img')) {
+    let realSrc = img.dataset.src;
+    if (!realSrc) continue;
+    if (isVisible(img)) {
+      // отключение кеширования
+      // эта строка должна быть удалена в "боевом" коде
+      realSrc += '?nocache=' + Math.random();
+      img.src = realSrc;
+      img.dataset.src = '';
+    }
+  }
+}
+window.addEventListener('scroll', showVisible);
+showVisible();
+// =====================================================================================
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".first",
+		start: "center 100%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.first', {
+  opacity: 0,
+  duration: 1
+})
+.to('.first', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".second",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.second', {
+  opacity: 0,
+  duration: 1
+})
+.to('.second', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".third",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.third', {
+  opacity: 0,
+  duration: 1
+})
+.to('.third', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".fourth",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.fourth', {
+  opacity: 0,
+  duration: 1
+})
+.to('.fourth', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".fifth",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.fifth', {
+  opacity: 0,
+  duration: 1
+})
+.to('.fifth', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".sixth",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.sixth', {
+  opacity: 0,
+  duration: 1
+})
+.to('.sixth', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".seventh",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.seventh', {
+  opacity: 0,
+  duration: 1
+})
+.to('.seventh', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".eighth",
+		start: "top 80%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.eighth', {
+  opacity: 0,
+  duration: 1
+})
+.to('.eighth', {
+  opacity: 1
+})
+
+gsap.timeline({
+	scrollTrigger: {
+		trigger: ".footer",
+		start: "top 200%",
+		markers: false,
+		scrub: false,
+		pin: false
+	}
+})
+.from('.footer', {
+  opacity: 0,
+  duration: 1
+})
+.to('.footer', {
+  opacity: 1
+})
 // =====================================================================================
 new Swiper('.first-slider',{
 
@@ -102,6 +277,13 @@ new Swiper('.first-slider',{
     delay: 5000,
   },
 
+  // зміна прозорості
+  effect: 'fade',
+  fadeEffect: {
+    // паралельна зміна прозорості
+    crossFade: true,
+  },
+
 });
 // =====================================================================================
 new Swiper('.seventh-slider',{
@@ -169,6 +351,13 @@ new Swiper('.seventh-slider',{
 
   // скорість переключення слайдів
   speed: 300,
+
+   // зміна прозорості
+   effect: 'fade',
+   fadeEffect: {
+     // паралельна зміна прозорості
+     crossFade: true,
+   },
 
   thumbs: {
     swiper: {
